36bfa804e796f8afbfa54dfb09dfe2483f493b9c
Peter Phanouvong <peterphanouvong@gmail.com>
2024-08-19 19:13:13 +1000

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
-import Link from "next/link";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser, getOrganization } = getKindeServerSession();
+
+  const user = await getUser();
+  const org = await getOrganization();
   return (
     <div className="container">
-      <div className="card hero">
-        <p className="text-display-1 hero-title">
-          Let’s start authenticating <br /> with KindeAuth
-        </p>
-        <p className="text-body-1 hero-tagline">Configure your app</p>
+      <ul>
+        <li>
+          <LoginLink postLoginRedirectURL="/api/auth/setup">Sign in</LoginLink>
+        </li>
+        <li>
+          <LoginLink orgCode="org_60dd4b10057c0">
+            Sign in to Test Tenant
+          </LoginLink>
+        </li>
+        <li>
+          <LoginLink orgCode="org_39299c145dd64">Sign in to Test8</LoginLink>
+        </li>
+        <li>
+          <LogoutLink>Sign out</LogoutLink>
+        </li>
+      </ul>
 
-        <Link
-          href="https://kinde.com/docs/sdks/nextjs-sdk"
-          target="_blank"
-          rel="noreferrer"
-          className="btn btn-light btn-big"
-        >
-          Go to docs
-        </Link>
-      </div>
+      {user && (
+        <div>
+          <h3>You are logged into {org?.orgCode}</h3>{" "}
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
